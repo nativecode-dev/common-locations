39e210db5fee9c638778cf4d691ddb58e87193f6
Mike Pham <contact@mikepham.com>
2017-02-10 10:28:18 -0500

    removing TODO for mkdir call

--- src/pathify.js
+++ src/pathify.js
@@ -3,7 +3,7 @@ const path = require('path')
 module.exports = fs => {
   return (directory, ...args) => {
     let current = directory
-    // TODO: make directories if they don't exist, one by one.
+
     args.forEach(arg => {
       current = path.join(current, arg)
 
